705ba22e25c2e21fa0734509408e604fc7791f11
simon <simon.landis@gmail.com>
2017-01-17 19:18:24 -0600

    database structure

---
+++ TrashCollector/Models/EmployeeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class EmployeeModel
+    {
+        public int id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Street { get; set; }
+        public string Steet_2 { get; set; }
+        [Required]
+        public string Zip { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+    }
+}
