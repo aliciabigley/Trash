7e99797fb78e1fbdffee5dbb856ce5c571de7943
simon <simon.landis@gmail.com>
2017-01-18 11:17:21 -0600

    added fkey customer

--- TrashCollector/Models/RouteViewModel.cs
+++ TrashCollector/Models/RouteViewModel.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using TrashCollector.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Core;
+using static TrashCollector.Models.CustomerModel;
 namespace TrashCollector.Models
 {
     public class RouteViewModel
     {
+        [Key]
         public int id { get; set; }
+        [Required]
+        public int Customerid { get; set; }
+        public virtual CustomerModel Customer { get; set; }
        
     }
 }
